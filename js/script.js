f7ca4850ef4085e73af4784186f23711d77898ef
GazaliMarif <97424315+GazaliMarif@users.noreply.github.com>
2022-03-18 05:52:40 -0700

    menambahkan file Js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var nilai = prompt('Masukan Nilai :')
+
+if(nilai <40 ){
+    alert ('nilai anda E')
+}
+
+else if(nilai <55 ){
+    alert ('nilai anda D')
+}
+
+else if(nilai < 60){
+    alert ('nilai anda C')
+}
+
+else if(nilai <70 ){
+    alert ('nilai anda B')
+}
+
+else if(nilai <81 ){
+    alert ('nilai anda AB')
+}
+
+else if(nilai <101 ){
+    alert ('nilai anda A')
+}
+
+else {
+    alert ('anda salah input')
+}
